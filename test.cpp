37239c3dc87a67d4b12028b95dfcd2f969e1ac64
AsadaSinon1 <103051656+AsadaSinon1@users.noreply.github.com>
2022-06-27 11:23:39 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <string>
+#include <io.h>
+using namespace std;
+bool initial_test()
+{
+    string command;
+    command = "mkdir ";
+    if (_access("saves", 0) == -1)
+        system((command + "saves").c_str());
+
+    /*if (_access("rules", 0) == -1)
+        return false;*/
+    // rules文件夹构建中
+    return true;
+}
